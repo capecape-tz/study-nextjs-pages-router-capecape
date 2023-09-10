bd8a3ffdc40f22e4abdd047da54bd74208703898
capecape-tz <capecape.tzgmail.com>
2023-09-11 00:13:38 +0900

    🧑‍💻 ULの色を少し変更

--- components/Navi.tsx
+++ components/Navi.tsx
@@ -4,14 +4,14 @@ import { usePathname } from "next/navigation";
 // const ACTIVE_ROUTE = "";
 // const INACTIVE_ROUTE = "";
 
-const ACTIVE_ROUTE = "px-4 py-4 text-gray-100 bg-gray-700";
+const ACTIVE_ROUTE = "px-4 py-4 text-gray-100 bg-gray-500";
 const INACTIVE_ROUTE =
   "px-4 py-4 text-gray-500 hover:text-gray-300 hover:bg-gray-700";
 
 export function Navi() {
   const pathname = usePathname();
   return (
-    <ul className="bg-red-700 border-8 border-red-900">
+    <ul className="bg-red-100 border-8 border-red-900">
       <Link href="/">
         <li className={pathname === "/" ? ACTIVE_ROUTE : INACTIVE_ROUTE}>
           トップへ移動する
