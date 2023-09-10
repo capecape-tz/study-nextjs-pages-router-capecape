0218100b00aaff1427a0cd28a20c642775697d3b
capecape-tz <capecape.tzgmail.com>
2023-09-11 02:00:03 +0900

    useCallbackを使用したイベントを作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,11 +4,21 @@ import { Footer } from "@/components/Footer";
 import { Links } from "@/components/Links";
 import { Headline } from "@/components/Headline";
 // import { Main } from "@/components/Main";
-import { ReactElement } from "react";
+import { MouseEvent, useCallback } from "react";
+
+import { MouseEventHandler, ReactElement } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const foo = "foo";
+  // const handleClick = (e: any) => {
+  //   console.log(foo);
+  // };
+  const handleClick = useCallback((e: any) => {
+    console.log(foo, "callback");
+  }, []);
+
   const page: string = "index";
 
   const codeComp: ReactElement = (
@@ -18,6 +28,9 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
+      <button onClick={handleClick} className="py-2 px-4">
+        onClickテストのボタン
+      </button>
       <Headline
         title="トップページ"
         page="index"
