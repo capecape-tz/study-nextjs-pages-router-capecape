1e12c26da3d8f4b1c6c64e2bd0f508db5230b07e
capecape-tz <capecape.tzgmail.com>
2023-09-10 19:01:30 +0900

    Footer部分とLinks部分をコンポーネント化

---
+++ components/Footer.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+export default function Footer() {
+  return (
+    <>
+      <div className="flex py-4 px-2 bg-green-500 text-black">フッターです</div>
+      ;
+    </>
+  );
+}
