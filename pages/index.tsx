eb8362887924bd745d42e8cebf0fbff095d679ec
capecape-tz <capecape.tzgmail.com>
2023-09-12 02:30:26 +0900

    🚸 PostsのAPIローディングなどを追加。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import { Inter } from "next/font/google";
-import { Footer } from "@/components/Footer";
-import { Main } from "@/components/Main";
 import { Navi } from "@/components/Navi";
 
-import { useState, useEffect, useCallback, ReactNode } from "react";
+import { ReactNode } from "react";
 import Head from "next/head";
-import { resolveAny } from "dns/promises";
+import Posts from "@/components/Posts";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,60 +21,14 @@ type Props = {
   children: ReactNode;
 };
 
-type Post = {
-  userId: string;
-  id: string;
-  title: string;
-  body: string;
-};
-
 export default function Home(props: Props) {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) {
-        throw new Error();
-      }
-      const json = await res.json();
-      setPosts(json);
-    } catch (e) {
-      setError("エラーが発生したため、データの取得に失敗しました。");
-    } finally {
-      setLoading(false);
-    }
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
   return (
     <div>
       <Head>
         <title>Index Page</title>
       </Head>
       <Navi></Navi>
-      {loading ? (
-        <div>ローディング中です</div>
-      ) : error ? (
-        <div>{error}</div>
-      ) : posts.length > 0 ? (
-        <ol>
-          {posts.map((post, pIndex) => {
-            return (
-              <li key={post.id}>
-                {post.id}:{post.title}
-              </li>
-            );
-          })}
-        </ol>
-      ) : (
-        <div>データは空です。</div>
-      )}
+      <Posts></Posts>
     </div>
   );
 }
