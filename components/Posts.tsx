96a0a9341f77c129672c5bdbad59bb129ac0e8e3
capecape-tz <capecape.tzgmail.com>
2023-09-12 14:52:28 +0900

    🎨 swrを導入。

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,93 +1,28 @@
-import { useReducer, useEffect, useCallback } from "react";
-
-type Post = {
-  userId: string;
-  id: string;
-  title: string;
-  body: string;
-};
-
-type State = {
-  data: Post[];
-  loading: boolean;
-  error: string | null;
-};
-
-type Action = {
-  type: "end" | "error" | null;
-  data: Post[];
-  error: string | null;
-};
-
-const initialState: State = {
-  data: [],
-  loading: true,
-  error: null,
-};
-
-const reducer = (state: State, action: Action) => {
-  switch (action.type) {
-    case "end":
-      return {
-        ...state,
-        data: action.data,
-        loading: false,
-      };
-    case "error":
-      return {
-        ...state,
-        error: action.error,
-        loading: false,
-      };
-    default: {
-      throw new Error("action typeがありませんでした");
-    }
-  }
-};
+import { usePosts } from "@/hooks/usePosts";
 
 export default function Posts() {
-  // const [posts, setPosts] = useState<Post[]>([]);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState<string | null>(null);
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const { data, error, isLoading } = usePosts();
 
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) {
-        throw new Error();
-      }
-      const json = await res.json();
-      dispatch({ type: "end", data: json, error: null });
-    } catch (e) {
-      dispatch({
-        type: "error",
-        data: [],
-        error: "エラーが発生したため、データの取得に失敗しました。",
-      });
-    }
-  }, []);
-
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
-
-  console.log("foo");
-  if (state.loading) {
+  console.log("aaa");
+  if (isLoading) {
     return <div>ローディング中です</div>;
   }
 
-  if (state.error) {
-    return <div>{state.error}</div>;
+  if (error) {
+    return <div>{error}</div>;
   }
 
-  if (state.data.length === 0) {
+  if (data === undefined) {
+    console.log("test");
+    console.log(data);
     return <div>データは空です。</div>;
   }
+  console.log("test2");
+  console.log(data);
 
   return (
     <ol>
-      {state.data.map((post, pIndex) => {
+      {data.map((post: any) => {
         return (
           <li key={post.id}>
             {post.id}:{post.title}
---
+++ hooks/usePosts.tsx
@@ -0,0 +1,25 @@
+import useSWR from "swr";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+const fetcher = async (url: string) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    return [];
+  }
+  const json = await response.json();
+  return json;
+};
+
+export function usePosts() {
+  const { data, error, isLoading } = useSWR<Post[]>(
+    "https://jsonplaceholder.typicode.com/posts",
+    fetcher
+  );
+  return { data, error, isLoading };
+}
