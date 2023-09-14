6367dd0af716c68b167b271d242a33e2b8a9a947
capecape-tz <capecape.tzgmail.com>
2023-09-14 12:37:56 +0900

    ✨ userに投稿一覧情報を表示

--- components/User.tsx
+++ components/User.tsx
@@ -1,3 +1,4 @@
+import { usePosts } from "@/hooks/useFetchArray";
 import { useUser } from "@/hooks/useUser";
 
 type Props = {
@@ -7,26 +8,52 @@ type Props = {
 export default function User(props: Props) {
   const { data: user, error, isLoading } = useUser(props.id);
 
-  if (isLoading) {
+  const {
+    data: posts,
+    error: errorPosts,
+    isLoading: isLoadingPosts,
+    isEmpty: isEmptyPosts,
+  } = usePosts();
+
+  if (isLoading || isLoadingPosts) {
     return <div>ローディング中です</div>;
   }
 
-  if (error) {
+  if (error || errorPosts) {
     return <div>{error}</div>;
   }
 
-  if (user === undefined) {
+  if (user === undefined || posts === undefined) {
     return <div>データは空です。</div>;
   }
 
   return (
-    <div>
-      <h1>
-        <div>{user?.id}</div>
-
-        {user?.name}
-      </h1>
-      <p>{user?.company.name}</p>
-    </div>
+    <>
+      <div>
+        <h1>
+          ユーザー情報
+          <div>{user?.id}</div>
+          {user?.name}
+        </h1>
+        <p>{user?.company.name}</p>
+      </div>
+      <h2>投稿</h2>
+      <ul>
+        {posts
+          ?.filter((post) => {
+            return post.userId === user.id;
+          })
+          .map((post) => {
+            return (
+              <>
+                <li key={post.id}>
+                  <div>{post.id}</div>
+                  <div>{post.body}</div>
+                </li>
+              </>
+            );
+          })}
+      </ul>
+    </>
   );
 }
